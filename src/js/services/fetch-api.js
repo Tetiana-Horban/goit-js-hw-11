7adb4e9fd441a115f790521cde65b3ce169ba84a
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-11-19 19:44:10 +0200

    without axios

--- src/js/services/fetch-api.js
+++ src/js/services/fetch-api.js
@@ -5,20 +5,28 @@ const OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true';
 export default class NewsApiService {
   constructor() {
     this.searchImages = '';
-    this.page = '';
+    this.page = 1;
+    this.per_page = 3;
+    this.totalHits = 1;
   }
 
   fetchImages() {
-    return fetch(`${BASE_URL}?${KEY}&q${this.searchImages}&${OPTIONS}&page=${this.page}`)
-      .then(response =>
-        // if (!response.ok) {
-        //   throw Error(response.statusText);
-        // }
-        response.json(),
-      )
-      .then(({ cards }) => {
+    const url = `${BASE_URL}?key=${KEY}&q=${this.searchImages}&${OPTIONS}&page=${this.page}&per_page=${this.per_page}`;
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.hits.length === 0) {
+          return Notiflix.Notify.failure(
+            `Sorry, there are no images matching your search query. Please try again.`,
+          );
+        }
         this.incrementPage();
-        return cards;
+        return data.hits;
       });
   }
 
