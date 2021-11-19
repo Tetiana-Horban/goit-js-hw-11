1a24495cd0ffa46f37535bef95b97bd952c959ca
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-11-19 19:43:40 +0200

    add LoadmoreButton and simplelightbox

--- src/index.js
+++ src/index.js
@@ -5,29 +5,65 @@ import NewsApiService from './js/services/fetch-api';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import galleryCards from './templates/gallery-cards.hbs';
+import LoadMoreBtn from './js/components/load-more-button';
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
-// const input = document.querySelector('[type=text]');
-// const button = document.querySelector('[type=submit]');
 
+const buttonLoadMore = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
 const newsApiService = new NewsApiService();
+const lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  nav: true,
+  close: true,
+});
+
+let totalHits = 0;
 
 searchForm.addEventListener('submit', onSearch);
+buttonLoadMore.refs.button.addEventListener('click', onLoadMore);
 
 function onSearch(event) {
   event.preventDefault();
-  newsApiService.query = event.currentTarget.elements.query.value;
-  fetchGallery();
-}
 
-function fetchGallery() {
-  newsApiService.fetchGallery().then(images => {
-    appendGalleryMarkup(images);
+  newsApiService.searchImages = event.currentTarget.elements.searchQuery.value.trim();
+  newsApiService.resetPage();
+  if (newsApiService.searchImages === '') {
+    return Notiflix.Notify.warning(`Oops! You need to enter some value`);
+  }
+  buttonLoadMore.show();
+
+  newsApiService.fetchImages().then(hits => {
+    clearGalleryConteiner();
+    appendGalleryMarkup(hits);
   });
 }
-function appendGalleryMarkup(images) {
-  gallery.insertAdjacentHTML('beforeend', gallerycards(images));
+
+function appendGalleryMarkup(hits) {
+  gallery.insertAdjacentHTML('beforeend', galleryCards(hits));
+  lightbox.refresh();
+  totalHits += hits.length;
+
+  Notiflix.Notify.success(`Hooray! We found ${totalHits} images for you`);
+}
+
+function onLoadMore() {
+  newsApiService.fetchImages().then(appendGalleryMarkup);
+}
+
+function clearGalleryConteiner() {
+  clearTotalHits();
+  gallery.innerHTML = '';
+}
+
+function clearTotalHits() {
+  totalHits = 0;
 }
 // const getContactId = searchImage => {
 //   return axios.get(``);
